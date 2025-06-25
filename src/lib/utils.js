eadaa1381c99f5212cf0f81c4688a6bd7a28bb4b
Sarfraz1115 <syedsarfraz907@gmail.com>
2025-06-25 08:45:46 +0530

    token

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,11 +5,12 @@ export const generateToken = (userID, res) => {
         expiresIn: "7d"
     })
 
-    res.cookie("jwt", token, {
+    res.cookie("token", token, {
         httpOnly: true,  // prevent XSS attacks cross-site scripting attacks
-        secure: process.env.NODE_ENV !== "development",
+        // secure: process.env.NODE_ENV !== "development",
+        secure: true,  // Use secure cookies in production
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        sameSite: "strict"  // CSRF attacks cross-site request forgery attacks
+        sameSite: "none"  // CSRF attacks cross-site request forgery attacks
     })
     return token;
 }
